c3a4bb4f88bf7f4fa725e1a812252575d5f2ae58
Rob Schoening <rschoening@lendingclub.com>
2017-09-12 07:18:34 -0700

    make regions configurable

--- mercator-demo/README.md
+++ mercator-demo/README.md
@@ -1,8 +1,9 @@
 
 ```
 docker run \
+  -v ~/.aws:/root/.aws
   -v /var/run/docker.sock:/var/run/docker.sock \
   -p 7474:7474 \
   -p 7687:7687 \
-  mercator-demo
+  lendingclub/mercator-demo
 ````
--- mercator-demo/src/main/java/org/lendingclub/mercator/demo/Main.java
+++ mercator-demo/src/main/java/org/lendingclub/mercator/demo/Main.java
@@ -15,53 +15,80 @@
  */
 package org.lendingclub.mercator.demo;
 
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.lendingclub.mercator.aws.AWSScannerBuilder;
 import org.lendingclub.mercator.aws.AllEntityScanner;
 import org.lendingclub.mercator.core.BasicProjector;
 import org.lendingclub.mercator.core.Projector;
 import org.lendingclub.mercator.docker.DockerScannerBuilder;
+import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import com.amazonaws.regions.Regions;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 public class Main {
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		SLF4JBridgeHandler.removeHandlersForRootLogger();
 		SLF4JBridgeHandler.install();
 		Projector projector = new Projector.Builder().build();
-		
+
 		Runnable awsTask = new Runnable() {
-			
+
 			public void run() {
-				projector.createBuilder(AWSScannerBuilder.class).withRegion(Regions.US_EAST_1).build(AllEntityScanner.class).scan();
-				projector.createBuilder(AWSScannerBuilder.class).withRegion(Regions.US_WEST_2).build(AllEntityScanner.class).scan();
+				try {
+				List<String> regionList = Splitter.on(",").omitEmptyStrings().trimResults()
+						.splitToList(Strings.nullToEmpty(System.getenv("AWS_REGIONS")));
+			
+				if (regionList==null || regionList.isEmpty()) {
+					regionList = Lists.newArrayList();
+					regionList.add("us-west-2");
+					regionList.add("us-east-1");
+				}
+				LoggerFactory.getLogger(Main.class).info("scanning regions: {}",regionList);
+				regionList.forEach(it -> {
+					try {
+						org.slf4j.LoggerFactory.getLogger(Main.class).info("scanning region: {}",it);
+						Regions region = Regions.fromName(it);
+						projector.createBuilder(AWSScannerBuilder.class).withRegion(region)
+								.build(AllEntityScanner.class).scan();
+					} catch (Exception e) {
+						LoggerFactory.getLogger(Main.class).warn("problem scanning " + it, e);
+					}
+				});
+				}
+				catch (Exception e) {
+					LoggerFactory.getLogger(Main.class).warn("problem",e);
+				}
 			}
-		
+
 		};
-		
+
 		Runnable dockerTask = new Runnable() {
 			public void run() {
-		
-		projector.createBuilder(DockerScannerBuilder.class).withLocalDockerDaemon().build().scan();
+
+				projector.createBuilder(DockerScannerBuilder.class).withLocalDockerDaemon().build().scan();
 			}
 		};
-		
-		ScheduledExecutorService exec = Executors.newScheduledThreadPool(2);
+
+		ScheduledExecutorService exec = Executors.newScheduledThreadPool(5);
 		exec.scheduleWithFixedDelay(awsTask, 0, 1, TimeUnit.MINUTES);
 		exec.scheduleWithFixedDelay(dockerTask, 0, 10, TimeUnit.SECONDS);
-		
 
-        while (true==true)  {
-            try {
-                Thread.sleep(1000);
-            }
-            catch (Exception e) {}
-        }
+		while (true == true) {
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+			}
+		}
 	}
 
 }
